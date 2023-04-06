f0cac15ddb0072cf758a05e7e9ac27a27cf3ec44
Prince Muchiri <110984211+PrinceMuchiri@users.noreply.github.com>
2023-04-06 22:24:06 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,68 @@
-from fabric.api import env, run, put
+#!/usr/bin/python3
+'''
+fabric script to distribute an archive to web servers
+----NEEDS TO REVISIT SCRIPT
+'''
+
 import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
 
 
-env.user = 'ubuntu'  # or whatever user you are using to connect to the servers
-env.hosts = ['	35.153.255.181','34.207.155.20']  # replace with the actual IP addresses of your servers
+env.hosts = ['35.153.255.181', '34.207.155.20']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not os.path.isfile(archive_path):
-        print("Error: file doesn't exist")
-        return False
-
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        archive_name = os.path.basename(archive_path)
-        tmp_path = "/tmp/{}".format(archive_name)
-        put(archive_path, tmp_path)
-
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename without extension> on the web server
-        timestamp = archive_name.split('.')[0][-14:]
-        release_path = "/data/web_static/releases/{}/".format(timestamp)
-        run("sudo mkdir -p {}".format(release_path))
-        run("sudo tar -xzf {} -C {}"
-            .format(tmp_path, release_path))
-
-        # Delete the archive from the web server
-        run("sudo rm {}".format(tmp_path))
-
-        # Move the files from the release folder to the web_static folder
-        run("sudo mv {}/web_static/* {}/"
-            .format(release_path, release_path))
-
-        # Remove the web_static folder from the release folder
-        run("sudo rm -rf {}/web_static".format(release_path))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create a new symbolic link /data/web_static/current on the web server,
-        # linked to the new version of your code
-        run("sudo ln -s {} /data/web_static/current"
-            .format(release_path))
-
-        print("New version deployed!")
-        return True
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
     except Exception:
-        return False
+        output = None
+    return output
 
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
